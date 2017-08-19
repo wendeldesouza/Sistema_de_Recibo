8702f4f64e005428ea1471e6170e279441573b81
wendeldesouza <rolim_sk8@hotmail.com>
2017-08-19 14:22:37 -0300

    Atualização 19-08-2017

--- JS/func.js
+++ JS/func.js
@@ -1,4 +1,4 @@
-/* Máscaras ER */
+/* Máscaras TEL */
 function mascara(o,f){
     v_obj=o
     v_fun=f
@@ -22,6 +22,18 @@ window.onload = function(){
 	}
 }
 
+//Máscara do CEP
+function MM_formtCep(e,src,mask) {
+    if(window.event) { _TXT = e.keyCode; } 
+    else if(e.which) { _TXT = e.which; }
+    if(_TXT > 47 && _TXT < 58) { 
+  var i = src.value.length; var saida = mask.substring(0,1); var texto = mask.substring(i)
+  if (texto.substring(0,1) != saida) { src.value += texto.substring(0,1); } 
+     return true; } else { if (_TXT != 8) { return false; } 
+  else { return true; }
+    }
+}
+
 //VALIDAÇÃO CEP
 $(document).ready(function() {
 
@@ -31,7 +43,7 @@ $(document).ready(function() {
                 $("#cidade").val("");
                 $("#uf").val("");
             }
-            
+
             //Quando o campo cep perde o foco.
             $("#cep").blur(function() {
 
@@ -80,3 +92,69 @@ $(document).ready(function() {
                 }
             });
         });
+
+//Validação de E-mail
+function IsEmail(email){
+    var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
+    var check=/@[w-]+./;
+    var checkend=/.[a-zA-Z]{2,3}$/;
+    if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
+    else {return true;}
+}
+
+
+
+
+function mascaraMutuario(o, f) {
+            v_obj = o
+            v_fun = f
+            setTimeout('execmascara()', 1)
+        }
+        function execmascara() {
+            v_obj.value = v_fun(v_obj.value)
+        }
+
+        function cpfCnpj(v) {
+            //Remove tudo o que não é dígito
+            v = v.replace(/\D/g, "")
+            if (v.length <= 14) { //CPF
+                //Coloca um ponto entre o terceiro e o quarto dígitos
+                v = v.replace(/(\d{3})(\d)/, "$1.$2")
+                //Coloca um ponto entre o terceiro e o quarto dígitos
+                //de novo (para o segundo bloco de números)
+                v = v.replace(/(\d{3})(\d)/, "$1.$2")
+                //Coloca um hífen entre o terceiro e o quarto dígitos
+                v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+            } else { //CNPJ
+                //Coloca ponto entre o segundo e o terceiro dígitos
+                v = v.replace(/^(\d{2})(\d)/, "$1.$2")
+                //Coloca ponto entre o quinto e o sexto dígitos
+                v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+                //Coloca uma barra entre o oitavo e o nono dígitos
+                v = v.replace(/\.(\d{3})(\d)/, ".$1/$2")
+                //Coloca um hífen depois do bloco de quatro dígitos
+                v = v.replace(/(\d{4})(\d)/, "$1-$2")
+            }
+            return v
+        }
+
+//Validação de Data dd/mm/aaaa
+function mascaraData( campo, e )
+{
+    var kC = (document.all) ? event.keyCode : e.keyCode;
+    var data = campo.value;
+    
+    if( kC!=8 && kC!=46 )
+    {
+        if( data.length==2 )
+        {
+            campo.value = data += '/';
+        }
+        else if( data.length==5 )
+        {
+            campo.value = data += '/';
+        }
+        else
+            campo.value = data;
+    }
+}
